fa23af7036435434204e12be33c81c1c111b9943
BrandonAGr <BrandonAGr@gmail.com>
2013-01-22 23:14:01 -0600

    Add gamma correction

--- src/pong/spi_windows.go
+++ src/pong/spi_windows.go
@@ -11,12 +11,12 @@ type SpiBus struct {
 }
 
 func NewSpiBus(busFilePath string, busSpeedHz uint) *SpiBus {
-	log.Fatal("Not implemented on windows!")
+	log.Fatal("Spi not implemented on windows!")
 	return nil
 }
 
 // Write data to the bus
 func (bus *SpiBus) Write(data []byte) (n int, err error) {
-	log.Fatal("Not implemented on windows!")
+	log.Fatal("Spi not implemented on windows!")
 	return
 }
